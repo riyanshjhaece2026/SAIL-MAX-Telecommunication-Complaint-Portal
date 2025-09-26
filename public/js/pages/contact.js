1781e07fa5ce7e58a146926375f2cadb27b6ce00
aec.ece.riyanshranjanjha.2026.030 <aec.ece.riyanshranjanjha.2026.030@gmail.com>
2025-09-26 06:18:23 +0000

    Noe Done For Use

--- public/js/pages/contact.js
+++ public/js/pages/contact.js
@@ -3,8 +3,22 @@ export function loadContactPage() {
   content.innerHTML = `
     <h2>Contact Us</h2>
     <p>For any inquiries, please contact:</p>
-    <p><strong>R.K. Ravi</strong></p>
-    <p><strong>Phone:</strong> +91 8986874581</p>
-    <p><strong>Emergency:</strong> 1800-180-6030</p>
+    <p><strong>Name:</strong> Somesh Mishra</p>
+    <p><strong>Designation:</strong> Junior Engineering Associate</p>
+    <p><strong>Contact No.:</strong> 8299531211</p>
+    <br>
+    <p><strong>Name:</strong> Birendra Yadav</p>
+    <p><strong>Designation:</strong> Junior Engineer</p>
+    <p><strong>Contact No.:</strong> 8340511052</p>
+    <br>
+    <p><strong>Name:</strong> Jayant Kumar</p>
+    <p><strong>Designation:</strong> Senior Manager</p>
+    <p><strong>Contact No.:</strong> 8986874965</p>
+    <br>
+    <p><strong>Name:</strong> Ravi Kant Ravi</p>
+    <p><strong>Designation:</strong> Asst. Gen. Manager</p>
+    <p><strong>Contact No.:</strong> 8986874581</p>
+    <br>
+
   `;
 }
